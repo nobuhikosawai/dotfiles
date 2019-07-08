c50d984d520e5c1e0e622a96369a944a1e331368
Nobuiko Sawai <nobuhiko.sawai@gmail.com>
2019-07-08 14:37:04 +0900

    Update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,16 +6,20 @@
 (package-initialize)
 
 (require 'package)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org))))
+ '(package-selected-packages (quote (molokai-theme org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(load-theme 'molokai t)
