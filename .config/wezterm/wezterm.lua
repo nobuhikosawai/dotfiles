70eff4c9965e2df884d5b33b0822e64d4f011209
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-08-04 13:26:15 -0700

    feat(wezterm): update font and such

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,15 +13,21 @@ end
 -- This is where you actually apply your config choices
 
 config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 0.85
+-- config.window_background_opacity = 0.85
 
-config.font = wezterm.font_with_fallback { 'JetBrainsMono Nerd Font', 'Ubuntu Mono' }
+config.font = wezterm.font_with_fallback { {
+  family = 'JetBrainsMono Nerd Font',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=1' },
+}, 'Ubuntu Mono' }
 config.font_size = 10.5
+config.freetype_load_target = 'Light'
+config.freetype_render_target = 'HorizontalLcd'
 
 config.use_ime = true
 config.xim_im_name = 'fcitx'
 config.ime_preedit_rendering = 'System'
 
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
