e47a4bad1d99a17033df1d1c967ee24a05f369cb
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-10-04 12:53:49 -0700

    fix(nvim): indent line setting

--- .config/nvim/lua/plugins/indent-line.lua
+++ .config/nvim/lua/plugins/indent-line.lua
@@ -3,37 +3,17 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     dependencies = {
       {
-        'HiPhish/rainbow-delimiters.nvim',
+        "HiPhish/rainbow-delimiters.nvim",
         event = { "BufReadPre", "BufNewFile" },
         config = function()
-          require('rainbow-delimiters.setup')({})
-        end
+          require("rainbow-delimiters.setup")({})
+        end,
       },
     },
     main = "ibl",
-    config = function()
-      local highlight = {
-        -- "RainbowDelimiterRed",
-        -- "RainbowDelimiterYellow",
-        -- "RainbowDelimiterBlue",
-        -- "RainbowDelimiterOrange",
-        -- "RainbowDelimiterGreen",
-        -- "RainbowDelimiterViolet",
-        -- "RainbowDelimiterCyan",
-        "White"
-      }
-      local hooks = require "ibl.hooks"
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-          vim.api.nvim_set_hl(0, "White", { fg = "#939293" })
-      end)
-      vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup {
-        scope = { highlight = highlight },
-        indent = { char = '▏' },
-      }
-
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-    end,
+    opts = {
+      indent = { char = "▏" },
+    },
     event = { "BufReadPost", "BufNewFile" },
   },
 }
